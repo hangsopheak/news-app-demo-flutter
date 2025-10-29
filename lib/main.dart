5a0e0158bd6be1f63307420f3cba333384f4a82a
sopheak <hangsopheak.hod@gmail.com>
2025-10-29 20:57:11 +0700

    named route example

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:news_app_demo_flutter/features/onboarding/ui/onboarding_screen.dart';
 import 'package:news_app_demo_flutter/features/onboarding/utils/onboarding_util.dart';
 import 'package:news_app_demo_flutter/main_screen.dart';
+import 'package:news_app_demo_flutter/routes/app_routes.dart';
+import 'package:news_app_demo_flutter/shared/domain/model/article.dart';
 import 'package:news_app_demo_flutter/shared/theme/theme.dart';
+
+import 'features/article/ui/article_detail_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -17,6 +21,16 @@ class MainApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: NewsAppTheme.lightTheme,
       darkTheme: NewsAppTheme.darkTheme,
+      onGenerateRoute: (settings){
+        if (settings.name == AppRoutes.articleDetail) {
+          final article = settings.arguments as Article;
+          return MaterialPageRoute(
+            builder: (context) => ArticleDetailScreen(article: article),
+          );
+        }
+        return null; // Let Flutter handle other routes normally
+
+      },
       home: FutureBuilder<bool>(
         future: OnboardingUtil.isOnboardingCompleted(),
         builder: (context, snapshot) {
